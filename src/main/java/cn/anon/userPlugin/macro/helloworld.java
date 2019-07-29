8a39c86fe45d4593d4823e979c5c1163025bf541
Anon <838891219@qq.com>
2019-07-29 17:38:23 +0800

    Customize the Hello World macro: using parameters.

--- src/main/java/cn/anon/userPlugin/macro/helloworld.java
+++ src/main/java/cn/anon/userPlugin/macro/helloworld.java
@@ -11,8 +11,12 @@ import java.util.Map;
  * @since 2019-07-29 11:55
  **/
 public class helloworld implements Macro {
-    public String execute(Map map, String s, ConversionContext conversionContext) throws MacroExecutionException {
-        return " Hello World ";
+    public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
+        if (map.get("Name") != null) {
+            return ("<h1>Hello " + map.get("Name") + "!</h1>");
+        } else {
+            return "<h1>Hello World!<h1>";
+        }
     }
 
     public BodyType getBodyType() {
